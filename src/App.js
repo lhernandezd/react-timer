a17d7f6cbf8a5f544888e8d4c1d5ba31b758c2e2
Luis Hernández <ldhdv95@gmail.com>
2018-09-24 16:58:37 -0500

    Add gh-pages package

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Timers from './components/Timers';
 const App = (props) => {
   return (
     <div className="App">
-      <h1 className='header-app'>Cronometros</h1>
+      <h1 className='header-app'>Timers</h1>
       <Timers />
     </div>
   );
